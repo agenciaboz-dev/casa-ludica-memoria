f4bf3f33605d889ba569bdd35788670827d8ef31
nandobfer <nandobfer@yahoo.com.br>
2022-10-06 01:18:16 -0300

    fix: typo

--- src/colors.js
+++ src/colors.js
@@ -2,6 +2,6 @@ export const COLORS = {
     background: "#f6f6f6",
     primary: '#17255A',
     secondary: "#92DCE5",
-    // secondary: "#f4511e",
+    // primary: "#f4511e",
     terciary: '#97DB4F',
 }
